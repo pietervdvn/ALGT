63df1fe6b532edf509827dd5ed67dc8c5c65df0a
Pieter Vander Vennet <pietervdn@gmail.com>
2016-12-16 20:34:54 +0100

    Fuckit, I'm to lazy to keep these languages up to date

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-git pull
-
 mkdir .bin >/dev/null
 
 cp -r src/* .bin
@@ -11,4 +9,4 @@ ghc Main.hs -fno-warn-tabs
 cd ..
 cp .bin/Main ALGT
 
-./ALGT Examples/STFL.typesystem Examples/STFL.example t eval --line-by-line --step 
+./ALGT -h
