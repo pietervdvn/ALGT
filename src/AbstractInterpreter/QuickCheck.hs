45f0558d8221366b6c9ffe9c6c02b27860f49f39
Pietervdvn <pietervdvn@gmail.com>
2017-03-09 12:38:54 +0100

    Random parsetree generation

--- src/AbstractInterpreter/QuickCheck.hs
+++ src/AbstractInterpreter/QuickCheck.hs
@@ -1,36 +1,58 @@
-module AbstractSet.QuickCheck where
+module AbstractInterpreter.QuickCheck where
 
 {-  -}
 
 import Utils.Utils
+import Utils.ToString
 
 import TypeSystem
 import AbstractInterpreter.AbstractSet
 
+import Data.Maybe
+
 import System.Random
 import Control.Monad.State as State
 
+import AssetsHelper
 
 
-generateRandomPT	:: (Random random) => Syntax -> TypeName -> State random ParseTree
-generateRandomPT s tn r
-	= do	r1	<- getR
-		r2	<- getR
-		let select r choices
-				= choices !! (r `mod` length choices)
-		let intChoices	= [3,4,5,6,7,8,9,10,32,64,100,128,256,512]
-		let ints	= [0,1,2] ++  select r intChoices ++ [r2]
+randomParseTrees		:: (RandomGen random) => Int -> Syntax -> TypeName -> Int -> random -> ([ParseTree], random)
+randomParseTrees neededPts s tn depth r
+	= runState (replicateM neededPts $ _generateRandomPT s tn depth) r
+
+randomParseTree		:: (RandomGen random) => Syntax -> TypeName -> Int -> random -> (ParseTree, random)
+randomParseTree s tn depth r
+	= runState (_generateRandomPT s tn depth) r
+
+
+
+_generateRandomPT	:: (RandomGen random) => Syntax -> TypeName -> Int -> State random ParseTree
+_generateRandomPT s tn depth
+	= do	rInt	<- getR
+		intChoice	<- selectRand ([3,4,5,6,7,8,9,10,32,64,100,128,256,512]	:: [Int])
+		let ints	= [0,1,2, intChoice, rInt]
 		-- selects given builtin ++ choices
-		let builtInNms	= builtinSyntax |> fst |> fst3
-		rands		<- builtInNms |+> const getR
-		let bi nm	= builtinSyntax |> fst |> dropSnd3 & lookup nm
-					& fromMaybe (error $ "No such builtin: "++nm)
-					& select 
-					
+		rands		<- builtinSyntax |> fst |> dropSnd3 |+> selectRand'
+		let bi nm	= rands & lookup nm
+					& maybe (error $ "No such builtin: "++nm) (:[])
+		
+		let ass		= generateAbstractSet s "" tn & unfoldDepth s 3
+		as		<- selectRand ass
+		let pts		= toParsetree s (ints, bi) as
+		selectRand pts			
 
 
-getR		:: State random Int
+getR		:: (RandomGen random) => State random Int
 getR	= do	r	<- State.get
 		let (i, r') = next r
-		put r
+		put r'
 		return i
+
+selectRand'	:: (RandomGen random) => (x, [a]) -> State random (x, a)
+selectRand' (x, as)
+	= do	r	<- getR
+		return (x, as !! (r `mod` length as))
+
+
+selectRand	:: (RandomGen random) => [a] -> State random a
+selectRand as	= selectRand' ((), as) |> snd
